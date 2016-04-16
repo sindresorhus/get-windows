9ba3c37cfbb715c7fa4ab2c938c31fd6049c2a94
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2016-04-16 09:47:37 -0700

    Linux support

--- index.js
+++ index.js
@@ -3,30 +3,54 @@ const path = require('path');
 const childProcess = require('child_process');
 const pify = require('pify');
 const bin = path.join(__dirname, 'main');
+const xprop = "xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)";
 
 function parse(stdout) {
-	const parts = stdout.trim().split('\n');
+	if (process.platform === 'darwin') {
+		const parts = stdout.trim().split('\n');
 
-	return {
-		title: parts[0] || null,
-		id: Number(parts[1]),
-		app: parts[2],
-		pid: Number(parts[3])
-	};
+		return {
+			title: parts[0] || null,
+			id: Number(parts[1]),
+			app: parts[2],
+			pid: Number(parts[3])
+		};
+	} else if (process.platform === 'linux') {
+		const result = {};
+		stdout.trim().split('\n').forEach(row => {
+			if (row.indexOf('=') > -1) {
+				row = row.split('=');
+				result[row[0].trim()] = row[1].trim();
+			} else if (row.indexOf(':') > -1) {
+				row = row.split(':');
+				result[row[0].trim()] = row[1].trim();
+			}
+		});
+
+		return {
+			title: JSON.parse(result['_NET_WM_NAME(UTF8_STRING)']) || null,
+			id: parseInt(result['WM_CLIENT_LEADER(WINDOW)'].split('#').pop(), 16),
+			app: JSON.parse(result['WM_CLASS(STRING)'].split(',').pop()),
+			pid: parseInt(result['_NET_WM_PID(CARDINAL)'], 10)
+		};
+	}
+	throw new Error('OS X and Linux only');
 }
 
 module.exports = () => {
-	if (process.platform !== 'darwin') {
-		return Promise.reject(new Error('OS X only'));
+	if (process.platform === 'darwin') {
+		return pify(childProcess.execFile)(bin).then(parse);
+	} else if (process.platform === 'linux') {
+		return pify(childProcess.exec)(xprop).then(parse);
 	}
-
-	return pify(childProcess.execFile)(bin).then(parse);
+	return Promise.reject(new Error('OS X and Linux only'));
 };
 
 module.exports.sync = () => {
-	if (process.platform !== 'darwin') {
-		throw new Error('OS X only');
+	if (process.platform === 'darwin') {
+		return parse(childProcess.execFileSync(bin, {encoding: 'utf8'}));
+	} else if (process.platform === 'linux') {
+		return parse(childProcess.execSync(xprop, {encoding: 'utf8'}));
 	}
-
-	return parse(childProcess.execFileSync(bin, {encoding: 'utf8'}));
+	throw new Error('OS X and Linux only');
 };
