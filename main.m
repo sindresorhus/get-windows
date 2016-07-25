60912153a4be7ec364ef309e2629e6ada1f53d73
Sindre Sorhus <sindresorhus@gmail.com>
2016-07-25 16:44:52 +0200

    modernize some of the Objective-C code

--- main.m
+++ main.m
@@ -2,17 +2,17 @@
 
 int main() {
 	@autoreleasepool {
-		NSInteger frontmostAppPID = [[NSWorkspace sharedWorkspace] frontmostApplication].processIdentifier;
+		NSInteger frontmostAppPID = [NSWorkspace sharedWorkspace].frontmostApplication.processIdentifier;
 		NSArray* windows = CFBridgingRelease(CGWindowListCopyWindowInfo(kCGWindowListOptionOnScreenOnly | kCGWindowListExcludeDesktopElements, kCGNullWindowID));
 
 		for (NSDictionary* window in windows) {
-			NSInteger windowOwnerPID = [[window objectForKey:(id)kCGWindowOwnerPID] intValue];
+			NSInteger windowOwnerPID = [window[(id)kCGWindowOwnerPID] intValue];
 
 			if (windowOwnerPID == frontmostAppPID) {
-				NSString* title = [window objectForKey:(id)kCGWindowName];
-				NSInteger winId = [[window objectForKey:(id)kCGWindowNumber] intValue];
-				NSString* app = [window objectForKey:(id)kCGWindowOwnerName];
-				NSInteger pid = [[window objectForKey:(id)kCGWindowOwnerPID] intValue];
+				NSString* title = window[(id)kCGWindowName];
+				NSInteger winId = [window[(id)kCGWindowNumber] intValue];
+				NSString* app = window[(id)kCGWindowOwnerName];
+				NSInteger pid = [window[(id)kCGWindowOwnerPID] intValue];
 				printf("%s\n%ld\n%s\n%ld\n", title.UTF8String, winId, app.UTF8String, pid);
 				return 0;
 			}
