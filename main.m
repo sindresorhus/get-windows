41daae2ac9ef97eb5ff0e4525d6c7c33179fc7d0
Sindre Sorhus <sindresorhus@gmail.com>
2016-07-25 17:45:00 +0200

    workaround apps with weird windows like Chrome

--- main.m
+++ main.m
@@ -9,11 +9,25 @@ int main() {
 			NSInteger windowOwnerPID = [window[(id)kCGWindowOwnerPID] intValue];
 
 			if (windowOwnerPID == frontmostAppPID) {
+				// skip transparent windows, like with Chrome
+				if ([window[(id)kCGWindowAlpha] intValue] == 0) {
+					continue;
+				}
+
+				// skip tiny windows, like the Chrome link hover statusbar
+				int minWinSize = 50;
+				NSDictionary* winBounds = window[(id)kCGWindowBounds];
+				if ([winBounds[@"Height"] intValue] < minWinSize ||
+					[winBounds[@"Width"] intValue] < minWinSize) {
+					continue;
+				}
+
 				NSString* title = window[(id)kCGWindowName];
 				NSInteger winId = [window[(id)kCGWindowNumber] intValue];
 				NSString* app = window[(id)kCGWindowOwnerName];
 				NSInteger pid = [window[(id)kCGWindowOwnerPID] intValue];
 				printf("%s\n%ld\n%s\n%ld\n", title.UTF8String, winId, app.UTF8String, pid);
+
 				return 0;
 			}
 		}
