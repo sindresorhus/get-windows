cd0557a6bf20efa5d2e5add2882358438e446d66
Sebastián Ramírez <tiangolo@gmail.com>
2017-04-25 12:14:47 -0500

    Fix format for xo compliance

--- windows.js
+++ windows.js
@@ -3,32 +3,32 @@ const path = require('path');
 
 const ffi = require('ffi');
 const ref = require('ref');
-const wchar_t = require('ref-wchar');
+const wchar = require('ref-wchar');
 
 // Required by QueryFullProcessImageName
 // https://msdn.microsoft.com/en-us/library/windows/desktop/ms684880(v=vs.85).aspx
 const PROCESS_QUERY_LIMITED_INFORMATION = 0x1000;
 
-// ffi declarations for the C++ library and functions needed (User32.dll), using their "Unicode" (UTF-16) version
-const user32 = ffi.Library('user32', {
+// Create ffi declarations for the C++ library and functions needed (User32.dll), using their "Unicode" (UTF-16) version
+const user32 = new ffi.Library('user32', {
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633505(v=vs.85).aspx
-	'GetForegroundWindow': ['pointer', []],
+	GetForegroundWindow: ['pointer', []],
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633520(v=vs.85).aspx
-	'GetWindowTextW': ['int', ['pointer', 'pointer', 'int']],
+	GetWindowTextW: ['int', ['pointer', 'pointer', 'int']],
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633521(v=vs.85).aspx
-	'GetWindowTextLengthW': ['int', ['pointer']],
+	GetWindowTextLengthW: ['int', ['pointer']],
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633522(v=vs.85).aspx
-	'GetWindowThreadProcessId': ['uint32', ['pointer', 'uint32 *']],
+	GetWindowThreadProcessId: ['uint32', ['pointer', 'uint32 *']]
 });
 
-// ffi declarations for the C++ library and functions needed (Kernel32.dll), using their "Unicode" (UTF-16) version
-const kernel32 = ffi.Library('kernel32', {
+// Create ffi declarations for the C++ library and functions needed (Kernel32.dll), using their "Unicode" (UTF-16) version
+const kernel32 = new ffi.Library('kernel32', {
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms684320(v=vs.85).aspx
-	'OpenProcess': ['pointer', ['uint32', 'int', 'uint32']],
+	OpenProcess: ['pointer', ['uint32', 'int', 'uint32']],
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms724211(v=vs.85).aspx
-	'CloseHandle': ['int', ['pointer']],
+	CloseHandle: ['int', ['pointer']],
 	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms684919(v=vs.85).aspx
-	'QueryFullProcessImageNameW': ['int', ['pointer', 'uint32', 'pointer', 'pointer']],
+	QueryFullProcessImageNameW: ['int', ['pointer', 'uint32', 'pointer', 'pointer']]
 });
 
 module.exports = () => {
@@ -39,22 +39,22 @@ module.exports = () => {
 	// Allocate a buffer large enough to hold the window text as "Unicode" (UTF-16) characters (using ref-wchar)
 	// This assumes using the "Basic Multilingual Plane" of Unicode, only 2 characters per Unicode code point
 	// Include some extra bytes for possible null characters
-	const windowTextBuffer = Buffer.alloc(windowTextLength * 2 + 4);
-	// Write the window text to the buffer
-	const windowTextSize = user32.GetWindowTextW(activeWindowHandle, windowTextBuffer, windowTextLength + 2);
+	const windowTextBuffer = Buffer.alloc((windowTextLength * 2) + 4);
+	// Write the window text to the buffer (it returns the text size, but is not used here)
+	user32.GetWindowTextW(activeWindowHandle, windowTextBuffer, windowTextLength + 2);
 	// Remove trailing null characters
-	const windowTextBufferClean = ref.reinterpretUntilZeros(windowTextBuffer, wchar_t.size)
+	const windowTextBufferClean = ref.reinterpretUntilZeros(windowTextBuffer, wchar.size);
 	// The the text as a Javascript string
-	const windowTitle = wchar_t.toString(windowTextBufferClean);
+	const windowTitle = wchar.toString(windowTextBufferClean);
 
 	// Allocate a buffer to store the process ID
 	const processIdBuffer = ref.alloc('uint32');
-	// Write the process ID creating the window to the buffer
-	const threadId = user32.GetWindowThreadProcessId(activeWindowHandle, processIdBuffer);
-	// get the process ID as a number from the buffer
+	// Write the process ID creating the window to the buffer (it returns the thread ID, but is not used here)
+	user32.GetWindowThreadProcessId(activeWindowHandle, processIdBuffer);
+	// Get the process ID as a number from the buffer
 	const processId = ref.get(processIdBuffer);
 	// Get a "handle" of the process
-	let processHandle = kernel32.OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, processId);
+	const processHandle = kernel32.OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, false, processId);
 	// Set the path length to more than the Windows extended-length MAX_PATH length
 	const pathLengthBytes = 66000;
 	// Path length in "characters"
@@ -66,9 +66,9 @@ module.exports = () => {
 	// Write process file path to buffer
 	kernel32.QueryFullProcessImageNameW(processHandle, 0, processFileNameBuffer, processFileNameSizeBuffer);
 	// Remove null characters from buffer
-	const processFileNameBufferClean = ref.reinterpretUntilZeros(processFileNameBuffer, wchar_t.size);
+	const processFileNameBufferClean = ref.reinterpretUntilZeros(processFileNameBuffer, wchar.size);
 	// Get process file path as a string
-	const processPath = wchar_t.toString(processFileNameBufferClean);
+	const processPath = wchar.toString(processFileNameBufferClean);
 	// Get process file name from path
 	const processName = path.basename(processPath);
 	// Close the "handle" of the process
@@ -79,6 +79,6 @@ module.exports = () => {
 		// There doesn't seem to be a notion of "window ID" in Windows, so returning -1
 		id: -1,
 		app: processName,
-		pid: processId,
+		pid: processId
 	};
-}
+};
