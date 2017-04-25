0662672f284272985571a66135590943808f9eec
Sebastian Ramirez <tiangolo@gmail.com>
2017-04-25 09:34:03 -0500

    Add references and comments

--- windows.js
+++ windows.js
@@ -11,16 +11,23 @@ const PROCESS_QUERY_LIMITED_INFORMATION = 0x1000;
 
 // ffi declarations for the C++ library and functions needed (User32.dll), using their "Unicode" (UTF-16) version
 const user32 = ffi.Library('user32', {
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633505(v=vs.85).aspx
 	'GetForegroundWindow': ['pointer', []],
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633520(v=vs.85).aspx
 	'GetWindowTextW': ['int', ['pointer', 'pointer', 'int']],
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633521(v=vs.85).aspx
 	'GetWindowTextLengthW': ['int', ['pointer']],
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms633522(v=vs.85).aspx
 	'GetWindowThreadProcessId': ['uint32', ['pointer', 'uint32 *']],
 });
 
 // ffi declarations for the C++ library and functions needed (Kernel32.dll), using their "Unicode" (UTF-16) version
 const kernel32 = ffi.Library('kernel32', {
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms684320(v=vs.85).aspx
 	'OpenProcess': ['pointer', ['uint32', 'int', 'uint32']],
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms724211(v=vs.85).aspx
 	'CloseHandle': ['int', ['pointer']],
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms684919(v=vs.85).aspx
 	'QueryFullProcessImageNameW': ['int', ['pointer', 'uint32', 'pointer', 'pointer']],
 });
 
@@ -54,16 +61,23 @@ module.exports = () => {
 	const pathLengthChars = Math.floor(pathLengthBytes / 2);
 	// Allocate a buffer to store the path of the process
 	const processFileNameBuffer = Buffer.alloc(pathLengthBytes);
-	// Create a buffer contain
+	// Create a buffer containing the allocated size for the path, as a buffer as it must be writable
 	const processFileNameSizeBuffer = ref.alloc('uint32', pathLengthChars);
+	// Write process file path to buffer
 	kernel32.QueryFullProcessImageNameW(processHandle, 0, processFileNameBuffer, processFileNameSizeBuffer);
+	// Remove null characters from buffer
 	const processFileNameBufferClean = ref.reinterpretUntilZeros(processFileNameBuffer, wchar_t.size);
+	// Get process file path as a string
 	const processPath = wchar_t.toString(processFileNameBufferClean);
+	// Get process file name from path
 	const processName = path.basename(processPath);
+	// Close the "handle" of the process
+	kernel32.CloseHandle(processHandle);
 
 	return {
 		title: windowTitle,
-		id: 0,
+		// There doesn't seem to be a notion of "window ID" in Windows, so returning -1
+		id: -1,
 		app: processName,
 		pid: processId,
 	};
