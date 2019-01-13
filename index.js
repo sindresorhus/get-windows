f4e377ec215c740e89a84bc6c9ce65e5cbbbfecc
Timothy Soehnlin <tim@eaiti.com>
2019-01-13 18:33:22 -0500

    Cleaning up

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const parseJSON = text => {
 function getCmdWithArgs(platform) {
 	let cmd;
 	switch (platform) {
-		case 'win32': cmd = 'win32.exe'; break;
-		case 'linux': cmd = 'linux.js'; break;
-		case 'darwin': cmd = 'darwin'; break;
+		case 'win32': cmd = 'active-win-win32.exe'; break;
+		case 'linux': cmd = 'active-win-linux.js'; break;
+		case 'darwin': cmd = 'active-win-darwin'; break;
 		default:
 			throw new Error('macOS, Linux, and Windows only');
 	}
@@ -34,7 +34,7 @@ function getCmdWithArgs(platform) {
 
 module.exports = async (platform = process.platform) => {
 	const [cmd, args] = getCmdWithArgs(platform);
-	return parseJSON(await execFile(cmd, args));
+	return parseJSON(await execFile(cmd, args, { encoding: 'utf8' }));
 };
 
 module.exports.sync = (platform = process.platform) => {
