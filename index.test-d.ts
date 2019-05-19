2d93d4f06c85ac71764758d3b83a483874c073a2
Yanis Benson <yanis.benson@gmail.com>
2019-05-19 03:06:44 +0300

    bugfix + better type tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -9,26 +9,24 @@ const result = activeWin.sync();
 expectType<Result | undefined>(result);
 
 if (result) {
+	expectType<string>(result.title);
+	expectType<number>(result.id);
+	expectType<number>(result.bounds.x);
+	expectType<number>(result.bounds.y);
+	expectType<number>(result.bounds.width);
+	expectType<number>(result.bounds.height);
+	expectType<string>(result.owner.name);
+	expectType<number>(result.owner.processId);
+	expectType<string>(result.owner.path);
+	expectType<number>(result.memoryUsage);
 	if (result.platform === 'macos') {
 		expectType<MacOSResult>(result);
-
-		expectType<string>(result.title);
-		expectType<number>(result.id);
-		expectType<number>(result.bounds.x);
-		expectType<number>(result.bounds.y);
-		expectType<number>(result.bounds.width);
-		expectType<number>(result.bounds.height);
-		expectType<string>(result.owner.name);
-		expectType<number>(result.owner.processId);
 		expectType<number>(result.owner.bundleId);
-		expectType<string>(result.owner.path);
-		expectType<number>(result.memoryUsage);
 	} else if (result.platform === 'linux') {
 		expectType<LinuxResult>(result);
 		expectError(result.owner.bundleId);
 	} else {
 		expectType<WindowsResult>(result);
-		expectType<string>(result.owner.path);
 		expectError(result.owner.bundleId);
 	}
 }
