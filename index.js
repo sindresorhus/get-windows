bb7f93145d4db5e6c002f8687d37fc0523455540
Sindre Sorhus <sindresorhus@gmail.com>
2021-05-15 13:16:06 +0700

    Fix linting

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 module.exports = options => {
 	if (process.platform === 'darwin') {
-		return require('./lib/macos')(options);
+		return require('./lib/macos.js')(options);
 	}
 
 	if (process.platform === 'linux') {
-		return require('./lib/linux')(options);
+		return require('./lib/linux.js')(options);
 	}
 
 	if (process.platform === 'win32') {
-		return require('./lib/windows')(options);
+		return require('./lib/windows.js')(options);
 	}
 
 	return Promise.reject(new Error('macOS, Linux, and Windows only'));
@@ -18,15 +18,15 @@ module.exports = options => {
 
 module.exports.sync = options => {
 	if (process.platform === 'darwin') {
-		return require('./lib/macos').sync(options);
+		return require('./lib/macos.js').sync(options);
 	}
 
 	if (process.platform === 'linux') {
-		return require('./lib/linux').sync(options);
+		return require('./lib/linux.js').sync(options);
 	}
 
 	if (process.platform === 'win32') {
-		return require('./lib/windows').sync(options);
+		return require('./lib/windows.js').sync(options);
 	}
 
 	throw new Error('macOS, Linux, and Windows only');
