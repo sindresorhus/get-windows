ea84ada50186c259aeddf43fdc5121f36bbd43c3
Sindre Sorhus <sindresorhus@gmail.com>
2022-02-23 19:25:28 +0700

    Fix CI

--- index.test-d.ts
+++ index.test-d.ts
@@ -23,7 +23,7 @@ if (result) {
 
 	if (result.platform === 'macos') {
 		expectType<MacOSResult>(result);
-		expectType<number>(result.owner.bundleId);
+		expectType<string>(result.owner.bundleId);
 		expectType<string | undefined>(result.url);
 	} else if (result.platform === 'linux') {
 		expectType<LinuxResult>(result);
